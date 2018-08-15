59cc536c6177f2b3335d360a53e20ca81a653126
sjmc88 <42321267+sjmc88@users.noreply.github.com>
2018-08-14 20:40:32 -0400

    Update bartender.py

--- bartender.py
+++ bartender.py
@@ -361,7 +361,7 @@ class Bartender(MenuDelegate):
 		traceback.print_exc()
 
 
-if add a print like 'print("hello")'
+add 'print("hello")'
 bartender = Bartender()
 bartender.buildMenu(drink_list, drink_options)
 bartender.run()
